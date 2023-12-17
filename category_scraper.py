697e9cf521ed995062fd7215a6b2972eb93f142e
mystery000 <lkostyrka17@gmail.com>
2023-12-17 13:20:20 -0500

    Time schedule

---
+++ category_scraper.py
@@ -0,0 +1,5 @@
+def run_category_scraper():
+    pass
+
+if __name__ == "__main__":
+    run_category_scraper()    
---
+++ product_scraper.py
@@ -0,0 +1,5 @@
+def run_product_scraper():
+    pass
+
+if __name__ == "__main__":
+    run_product_scraper()    
